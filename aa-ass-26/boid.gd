a589ece22583c41dd207241ae7a6e782f7c64fb4
c233033483 <saraheithne@gmail.com>
2026-03-22 17:59:18 +0000

    Boid script pushing

--- aa-ass-26/boid.gd
+++ aa-ass-26/boid.gd
@@ -1,28 +1,21 @@
 extends CharacterBody3D
 
+@export var target:Node3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var speed = 2.0
+@export var acceleration:Vector3 = Vector3.ZERO
+@export var force:Vector3 = Vector3.ZERO
 
+@export var seek_enabled:bool = false
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	var currentForce = calculate_force()
+	 
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func calculate_force():
+	var f:Vector3
+	return f
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
 
-	move_and_slide()
+func arrive():
+	var to_target = target.global_position
