6c7267ef59447f961065f71baa1a92e53b3fad74
c233033483 <saraheithne@gmail.com>
2026-05-03 22:35:05 +0100

    Fixed spawning bug
    Replaced inspector drag-in with hard coded reference to the larva scene

--- aa-ass-26/Scripts/Egg.gd
+++ aa-ass-26/Scripts/Egg.gd
@@ -1,11 +1,10 @@
 class_name Egg extends RigidBody3D
 
 @export var hatch_time: float  = 20.0
-@export var larva_pref: Resource
 
 func _ready() -> void:
 	add_to_group("eggs")
-	print("Egg ready, larva_pref is: ", larva_pref)
+	print("Egg ready, larva_pref is: ", "res://Scenes/larva.tscn")
 	var timer := Timer.new()
 	timer.wait_time = hatch_time
 	timer.one_shot  = true
@@ -14,11 +13,9 @@ func _ready() -> void:
 	add_child(timer)
 
 func _hatch() -> void:
-	print("Hatching! larva_scene is: ", larva_pref)
-	if larva_pref == null:
-		print("No larva scene assigned to egg!")
-		return
-	var larva := (larva_pref as PackedScene).instantiate()
+	print("Hatching! larva_scene is: ", )
+	var larva_scene := load("res://Scenes/larva.tscn") as PackedScene
+	var larva := larva_scene.instantiate()	
 	var spawn_pos := global_position
 	get_parent().add_child(larva)
 	larva.global_position = spawn_pos
